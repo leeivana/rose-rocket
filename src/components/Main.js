54041c936b2385ff47ad4452daca0abf169dafe6
leeivana <lee.ivana@hotmail.com>
2019-01-14 14:01:44 -0500

    Added code to fetch data in Board component and displays all the stops and the corresponding stop name

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,7 @@
 import React, { Component } from 'react'; 
 import Board from './constants/Board';
 class MainPage extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      fetching: false,
-      haveData: false,
-      legs: '',
-      stops: '', 
-      driver: '',
-    }
-  }
-
-  componentDidMount = () => {
-    this.setState({fetching: true});
-    Promise.all([
-      fetch('http://localhost:3000/legs'),
-      fetch('http://localhost:3000/stops'),
-      fetch('http://localhost:3000/driver')
-    ])
-    .then(([res1, res2, res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
-    .then(([legs, stops, driver]) => this.setState({
-      legs, 
-      stops,
-      driver,
-      fetching: false,
-      haveData: true,
-    }))
-    .catch(err => console.warn('Error:', err))
-  }
   render(){
-    const { haveData, fetching, data } = this.state;
     return(
       <div>
         <Board />
--- src/components/constants/Board.js
+++ src/components/constants/Board.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Board.css';
 
-const CELL_SIZE = 20;
-const WIDTH = 800;
-const HEIGHT = 600;
-
+const CELL_SIZE = window.innerHeight/110;
+const WIDTH = CELL_SIZE * 50;
+const HEIGHT = window.innerHeight;
+class Cell extends React.Component {
+  render() {
+    const { x, y, stopName } = this.props;
+    console.log(stopName);
+    return (
+      <div data-before={stopName} className="Cell" style={{
+        left: `${CELL_SIZE * x + 1}px`,
+        top: `${CELL_SIZE * y + 1}px`,
+        width: `${CELL_SIZE - 1}px`,
+        height: `${CELL_SIZE - 1}px`,
+      }} />
+    );
+  }
+}
 class Board extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.rows = HEIGHT / CELL_SIZE;
     this.cols = WIDTH / CELL_SIZE;
     this.board = this.makeEmptyBoard();
@@ -15,6 +28,24 @@ class Board extends React.Component {
   state = {
     cells: [],
   }
+
+  componentDidMount = () => {
+    this.setState({fetching: true});
+    Promise.all([
+      fetch('http://localhost:3000/legs'),
+      fetch('http://localhost:3000/stops'),
+      fetch('http://localhost:3000/driver')
+    ])
+    .then(([res1, res2, res3]) => Promise.all([res1.json(), res2.json(), res3.json()]))
+    .then(([legs, stops, driver]) => this.setState({
+      legs: legs.legData.legData, 
+      cells: stops.stopData.stopData,
+      driver: driver.driverData.driverData,
+      fetching: false,
+    }))
+    .catch(err => console.warn('Error:', err))
+  }
+
   makeEmptyBoard() {
     let board = [];
     for (let y = 0; y < this.rows; y++) {
@@ -36,14 +67,24 @@ class Board extends React.Component {
     }
     return cells;
   }
-  
+
   render() {
+    const { cells } = this.state;
     return (
       <div>
         <div className="Board"
           style={{ width: WIDTH, height: HEIGHT,
-            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}>
+            backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}
+          ref={(n) => { this.boardRef = n; }}
+        >
+          {cells.map(cell => (
+            <Cell x={cell.x} y={cell.y}
+              key={`${cell.x},${cell.y}, ${cell.name}`}
+              stopName={cell.name}
+            />
+          ))}
         </div>
+        
       </div>
     );
   }
