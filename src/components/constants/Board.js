334c75c3d64a134a0173464d0a609dd789f58b9c
leeivana <lee.ivana@hotmail.com>
2019-01-14 02:18:37 -0500

    Added grid template

--- src/components/constants/Board.js
+++ src/components/constants/Board.js
@@ -6,6 +6,37 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.rows = HEIGHT / CELL_SIZE;
+    this.cols = WIDTH / CELL_SIZE;
+    this.board = this.makeEmptyBoard();
+  }
+  state = {
+    cells: [],
+  }
+  makeEmptyBoard() {
+    let board = [];
+    for (let y = 0; y < this.rows; y++) {
+      board[y] = [];
+      for (let x = 0; x < this.cols; x++) {
+        board[y][x] = false;
+      }
+    }
+    return board;
+  }
+  makeCells() {
+    let cells = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
+          cells.push({ x, y });
+        }
+      }
+    }
+    return cells;
+  }
+  
   render() {
     return (
       <div>
