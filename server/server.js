900fb85fe7302a2dee6f824d7e169035e4464c00
leeivana <lee.ivana@hotmail.com>
2019-01-17 21:11:13 -0500

    Added websocket setup to server.js

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ const fastify = require('fastify')({
   logger: true
 });
 const cors = require('cors');
-fastify.register(require('../src/routes'))
-fastify.use(cors())
-fastify.listen(3000, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  fastify.log.info(`server listening on ${address}`)
+fastify.register(require('fastify-ws'));
+fastify.register(require('../src/routes'));
+fastify.use(cors());
+
+fastify.ready(err => {
+  if (err) throw err
+
+  fastify.ws.on('connection', socket => {
+    console.log('connection')
+    socket.on('message', msg => {
+      console.log(msg)
+      socket.send(msg)
+    })
+  })
+})
+
+fastify.listen(3000, err => {
+  if (err) throw err
+  console.log('listening')
 })
