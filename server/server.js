77a1f3ec942798850ddfcbc82c21c726693ce661
leeivana <lee.ivana@hotmail.com>
2019-01-20 17:06:06 -0500

    Changed server port

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ fastify.ready(err => {
   })
 })
 
-fastify.listen(3000, err => {
+fastify.listen(3001, err => {
   if (err) throw err
-  console.log('listening on port 3000')
+  console.log('listening on port 3001')
 })
